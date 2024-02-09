ac9dac69158fef3067c6446c2271025d01a959d4
MiriamEvardi <miriamevardi97@gmail.com>
2024-02-09 16:53:45 +0100

    added "time is up" when the countdown expires

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-// Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
-// Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di lunedì mattina!
-// Consigli
-// Questo esercizio richiede un po' di ricerca ma anche un po' di domande che accompagnano l'uomo da tempo immemore:
-// Da quante ore è composto un giorno?
-// Da quanti minuti è composta un'ora?
-// Da quanti secondi è composto un minuto?
-// Da quanti millisecondi è composto un secondo?
-// Quanti millisecondi mi separano da domani alle 9:30?
-// Esiste un oggetto JS in grado di gestire le date?
-// Esistono dei metodi per trasformare una data in millisecondi?
-
-
-
 
 // data e ora di lunedì
-const mondayDate = new Date("February 9, 2024 16:43:00");
+const mondayDate = new Date("February 12, 2024 9:30:00");
+console.log(mondayDate)
 
 const timer = setInterval(function () {
     // data di oggi
@@ -40,7 +27,11 @@ const timer = setInterval(function () {
     document.getElementById("timer").innerText = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
 
 
-}, 0)
+    if (count < 0) {
+        clearInterval(timer);
+        document.getElementById("timer").innerText = "Time is up"
+    }
+}, 1000)
 
 
 
